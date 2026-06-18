45d017d45061adbee5bcfd976daf738cf8ad8568
Ariiq354 <realgoldcraft@gmail.com>
2026-06-18 20:30:45 +0700

    feat: ui update

--- app/features/auth/LoginContainer.vue
+++ app/features/auth/LoginContainer.vue
@@ -1,89 +1,71 @@
-<script setup lang="ts">
-import type { FormSubmitEvent } from "@nuxt/ui";
-import type { LoginSchema } from "./constants";
-import { initFormDataLogin, loginSchema } from "./constants";
-
-const state = reactive(initFormDataLogin);
-const isLoading = ref(false);
-
-const route = useRoute();
-async function onSubmit(event: FormSubmitEvent<LoginSchema>) {
-  const redirect = typeof route.query.redirect === "string"
-    ? route.query.redirect
-    : "/dashboard";
-
-  authClient.signIn.email({
-    email: event.data.email,
-    password: event.data.password,
-    rememberMe: event.data.rememberMe,
-  }, {
-    onRequest: () => {
-      isLoading.value = true;
-    },
-    onSuccess: async () => {
-      isLoading.value = false;
-      await navigateTo(redirect, { external: true });
-    },
-    onError: () => {
-      isLoading.value = false;
-      useToastError("Error", "Gagal keluar. Silahkan coba lagi.");
-    },
-  });
-}
-</script>
+<script setup></script>
 
 <template>
-  <div class="">
-    <NuxtImg src="/images/bg-auth.webp" />
-  </div>
-  <UCard class="w-full max-w-md">
-    <div class="space-y-6">
-      <div class="flex flex-col">
-        <div class="">
-          <NuxtImg src="/images/logo.webp" alt="logo-kompak98" class="w-50" />
-        </div>
-        <div class="text-highlighted text-2xl font-bold mt-6">
-          Welcome Back
-        </div>
-        <div class="text-muted mt-1">
-          Please sign in to access your alumni ntework.
-        </div>
-      </div>
-      <UForm
-        :schema="loginSchema"
-        :state="state"
-        class="w-full space-y-6"
-        @submit="onSubmit"
-      >
-        <UFormField label="Email" name="email">
-          <UInput
-            v-model="state.email"
-            :disabled="isLoading"
-            placeholder="Masukkan email anda"
-          />
-        </UFormField>
+  <div class="min-h-screen w-full flex">
+    <!-- Left Side -->
+    <div class="hidden lg:flex w-1/2 relative">
+      <NuxtImg
+        src="/images/auth-image.svg"
+        alt="Auth Banner"
+        class="absolute inset-0 w-full h-full object-cover"
+      />
+      <div class="absolute inset-0 bg-black/40" />
 
-        <UFormField label="Password" name="password">
-          <InputPassword
-            v-model="state.password"
-            :disabled="isLoading"
-            placeholder="Masukkan password anda"
-          />
-        </UFormField>
-
-        <UCheckbox
-          v-model="state.rememberMe"
-          label="Ingat saya"
-        />
-
-        <UButton
-          class="flex w-full justify-center"
-          type="submit"
-          :loading="isLoading"
-        >
-          Masuk
-        </UButton>
-      </UForm>
+      <NuxtImg
+        src="/images/logo-vertical.webp"
+        class="relative z-10 m-auto"
+        alt="Logo"
+      />
     </div>
-  </UCard>
+
+    <!-- Right Side -->
+    <div class="w-full lg:w-1/2 flex items-center justify-center bg-blue-50 px-6 py-12">
+      <UCard class="w-full max-w-md" title="Selamat Datang" description="Masukkan nip dan password Anda untuk mengakses akun Anda.">
+        <div class="space-y-6">
+          <div class="flex flex-col items-center text-center">
+            <NuxtImg src="/ppg.webp" width="200" height="200" alt="logo ppg" />
+            <div class="text-highlighted text-2xl font-bold">
+              Assalamualaikum
+            </div>
+            <div class="text-muted mt-1">
+              Silahkan login dengan akun anda
+            </div>
+          </div>
+          <UForm
+            class="w-full space-y-6"
+          >
+            <UFormField label="NIP 9" name="username">
+              <UInput
+                placeholder="Masukkan username anda"
+              />
+            </UFormField>
+
+            <UFormField label="Password" name="password">
+              <InputPassword
+                placeholder="Masukkan password anda"
+              />
+            </UFormField>
+
+            <UCheckbox
+              label="Ingat saya"
+            />
+
+            <UButton
+              class="flex w-full justify-center"
+              type="submit"
+            >
+              Masuk
+            </UButton>
+          </UForm>
+
+          <div class="font-bold text-muted text-sm text-center">
+            Untuk pendaftaran akun, hubungi <a
+              class="text-primary-500" href="https://wa.me/6289605834601"
+              target="_blank"
+            >Admin</a>
+          </div>
+        </div>
+      </UCard>
+    </div>
+  </div>
 </template>
