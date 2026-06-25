4d3db6cc4840b6eb8e7d002aec42bbe07fc05fc1
Ariiq354 <realgoldcraft@gmail.com>
2026-06-25 13:09:23 +0700

    feat: dashboard

---
+++ server/modules/dashboard/repo.ts
@@ -0,0 +1,57 @@
+import { and, eq, isNotNull, sql } from "drizzle-orm";
+import { db } from "~~/server/database";
+import { userTable } from "~~/server/database/schema/auth";
+import { pembayaranIuranKhususTable, pembayaranKasBulananTable } from "~~/server/database/schema/iuran";
+import { userProfileTable } from "~~/server/database/schema/user";
+
+export abstract class DashboardRepo {
+  static async getUserDashboard() {
+    const data = await db.select({
+      id: userTable.id,
+      gender: userProfileTable.gender,
+      provinsiKantor: userProfileTable.provinsiKantor,
+    })
+      .from(userTable)
+      .leftJoin(userProfileTable, eq(userTable.id, userProfileTable.userId))
+      .where(and(isNotNull(userProfileTable.gender), isNotNull(userProfileTable.provinsiKantor)));
+
+    const result = data.map(item => ({
+      id: item.id,
+      gender: item.gender!,
+      provinsiKantor: item.provinsiKantor!,
+    }));
+
+    return result;
+  }
+
+  static async getPemasukan() {
+    const currentYear = new Date().getFullYear();
+
+    const dataKhusus = await db.select({
+      total: sql<number>`coalesce(sum(${pembayaranIuranKhususTable.nominal}), 0)`,
+      tanggalBayar: pembayaranIuranKhususTable.tanggalBayar,
+    })
+      .from(pembayaranIuranKhususTable)
+      .where(
+        sql`extract(year from ${pembayaranIuranKhususTable.tanggalBayar}) = ${currentYear}
+        and ${pembayaranIuranKhususTable.status} = 'lunas'`,
+      )
+      .groupBy(pembayaranIuranKhususTable.tanggalBayar);
+
+    const dataBulanan = await db.select({
+      total: sql<number>`coalesce(sum(${pembayaranKasBulananTable.nominal}), 0)`,
+      tanggalBayar: pembayaranKasBulananTable.tanggalBayar,
+    })
+      .from(pembayaranKasBulananTable)
+      .where(
+        sql`extract(year from ${pembayaranKasBulananTable.tanggalBayar}) = ${currentYear}
+        and ${pembayaranKasBulananTable.status} = 'lunas'`,
+      )
+      .groupBy(pembayaranKasBulananTable.tanggalBayar);
+
+    return {
+      dataKhusus: dataKhusus.map(item => ({ total: item.total, tanggalBayar: item.tanggalBayar! })),
+      dataBulanan: dataBulanan.map(item => ({ total: item.total, tanggalBayar: item.tanggalBayar! })),
+    };
+  }
+}
---
+++ server/modules/dashboard/service.ts
@@ -0,0 +1,58 @@
+import provinsi from "~~/public/wilayah/provinces.json";
+import { months } from "~~/shared/constant";
+import { DashboardRepo } from "./repo";
+
+export abstract class DashboardService {
+  static async getDashboard() {
+    const user = await DashboardRepo.getUserDashboard();
+    const pemasukan = await DashboardRepo.getPemasukan();
+
+    const totalUser = user.length;
+    const totalLaki = user.filter(u => u.gender === "Laki-laki").length;
+    const totalPerempuan = user.filter(u => u.gender === "Perempuan").length;
+
+    const countProvinsi = user.map(u => u.provinsiKantor).reduce((acc, curr) => {
+      acc[curr] = (acc[curr] || 0) + 1;
+      return acc;
+    }, {} as Record<string, number>);
+
+    const chartProvinsi = provinsi.map(p => ({
+      provinsi: p.name,
+      total: countProvinsi[p.id] || 0,
+    }));
+
+    const result = months.map((month, index) => {
+      let total = 0;
+
+      pemasukan.dataKhusus.forEach((item) => {
+        if (item.tanggalBayar) {
+          const m = new Date(item.tanggalBayar).getMonth();
+          if (m === index)
+            total += Number(item.total);
+        }
+      });
+
+      pemasukan.dataBulanan.forEach((item) => {
+        if (item.tanggalBayar) {
+          const m = new Date(item.tanggalBayar).getMonth();
+          if (m === index)
+            total += Number(item.total);
+        }
+      });
+
+      return {
+        bulan: month,
+        pemasukan: total,
+        pengeluaran: Math.floor(Math.random() * 100000),
+      };
+    });
+
+    return {
+      totalUser,
+      totalLaki,
+      totalPerempuan,
+      chartProvinsi,
+      chartPemasukan: result,
+    };
+  }
+}
